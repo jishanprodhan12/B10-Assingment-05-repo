99679ac296ba58131b966382e8724cea8d2cad7d
jishanprodhan12 <mdjishanprodhan132@gmail.com>
2024-09-22 23:49:36 -0500

    noakhali dontate tk alert check

--- js/index.js
+++ js/index.js
@@ -4,7 +4,10 @@
 document.getElementById('noakhali-donate-btn').addEventListener('click',function(){
     const noakhaliDonateInput = getValueByInputFild('input-noakhali');
     const noakhaliBalance = getValueByTextFild('noakhali-balance');
+    const userBalance = getValueByTextFild('user-balance');
     const upadatedNoakhaliBalance = noakhaliBalance + noakhaliDonateInput ;
+    const updateUser = userBalance - noakhaliDonateInput;
     document.getElementById('noakhali-balance').innerText = upadatedNoakhaliBalance ;
-
+    document.getElementById('user-balance').innerText = updateUser;
+    alert(noakhaliDonateInput + " TK Donate for Flood at Noakhali, Bangladesh");
 });
