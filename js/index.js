b15d2d247184964af7b9ba641e8b40cb9d038a30
jishanprodhan12 <mdjishanprodhan132@gmail.com>
2024-09-22 23:35:09 -0500

    resolve utilites

---
+++ js/index.js
@@ -0,0 +1,9 @@
+
+
+
+document.getElementById('noakhali-donate-btn').addEventListener('click',function(){
+    const noakhaliDonateInput = getValueByInputFild('input-noakhali');
+    console.log(noakhaliDonateInput);
+    
+
+});
--- js/utilites.js
+++ js/utilites.js
@@ -1,17 +1,13 @@
 //function for get value using value 
 function getValueByInputFild (id){
-document.getElementById(id).addEventListener('click',function(event){
-    const result = event.target.value ;
+    const result = document.getElementById(id).value ;
     resultNumber = parseFloat(result);
     return resultNumber ;
-});
 }
 
 /// function for get value using innerText 
 function getValueByTextFild (id){
-    document.getElementById(id).addEventListener('click',function(event){
-        const result = event.target.innerText ;
-        resultNumber = parseFloat(result);
-        return resultNumber ;
-    });
-    }
+    const result = document.getElementById(id).innerText ;
+    resultNumber = parseFloat(result);
+    return resultNumber ;
+}
